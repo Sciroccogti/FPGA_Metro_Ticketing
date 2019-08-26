1d8abaf080fbfd7203f913b76963758f3ded05be
unknown <zyf199966@126.com>
2019-08-26 15:34:53 +0800

    add vivado

---
+++ FPGA_Metro_Ticketing.srcs/sources_1/new/top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2019/08/26 14:31:00
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+    input x
+    );
+endmodule
