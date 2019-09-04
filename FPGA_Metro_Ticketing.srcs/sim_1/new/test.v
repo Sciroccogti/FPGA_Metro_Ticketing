dc8fce597cd2f6a184cdcd3270a5ae26dbc44573
unknown <zyf199966@126.com>
2019-09-04 22:47:10 +0800

    第一次尝试

--- FPGA_Metro_Ticketing.srcs/sim_1/new/test.v
+++ FPGA_Metro_Ticketing.srcs/sim_1/new/test.v
@@ -1,11 +1,11 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// CompANy: 
+// Company: 
 // Engineer: 
 // 
-// Create Date: 2019/08/26 14:31:00
+// Create Date: 2019/09/04 21:48:39
 // Design Name: 
-// Module Name: top
+// Module Name: test
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,20 +20,12 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module top(
-    input CLK100MHZ,
-    input [15:0]SW,
-    output [6:0]SEG,
-    output [7:0]AN,
-    output DP
+module test(
+
     );
-
-    reg CLK50MHZ = 0;
-
-    always @(posedge(CLK100MHZ)) begin
-        CLK50MHZ <= ~CLK50MHZ;
-    end
-
+    wire [6:0] SEG;
+    wire [7:0] AN;
+    wire DP;
     wire [31:0] count;
     generate
         genvar i;
@@ -41,5 +33,5 @@ module top(
             assign count[i*4 + 3:i*4] = i + 1;
             end
     endgenerate
-    seg7decimal show (count[31:0], CLK100MHZ, SEG[6:0], AN[7:0], DP);
+    seg7decimal show (count[31:0], clk, SEG[6:0], AN[7:0], DP);
 endmodule
